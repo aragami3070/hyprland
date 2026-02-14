0908b071ca1db50dcc4095b5458f597fd0bd51b8
aragami3070 <smirnov17612@gmail.com>
2026-02-15 02:39:52 +0400

    Feat(setup-scripts): add symlinks setup

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -4,16 +4,38 @@
 set -euo pipefail
 
 # hypr
-# ln -s
+rm ~/.config/hypr -r
+ln -s ~/.config/hyprland/hypr ~/.config/hypr
 # mpv
+rm ~/.config/mpv -r
+ln -s ~/.config/hyprland/mpv ~/.config/mpv
 # pgcli
-# LoginTheme
+rm ~/.config/pgcli -r
+ln -s ~/.config/hyprland/pgcli ~/.config/pgcli
 # btop
+rm ~/.config/btop -r
+ln -s ~/.config/hyprland/btop ~/.config/btop
 # dunst
+rm ~/.config/dunst -r
+ln -s ~/.config/hyprland/dunst ~/.config/dunst
 # fastfetch
+rm ~/.config/fastfetch -r
+ln -s ~/.config/hyprland/fastfetch ~/.config/fastfetch
 # firefox
+rm ~/.config/firefox -r
+ln -s ~/.config/hyprland/firefox ~/.config/firefox
 # tmux
+rm ~/.config/tmux -r
+ln -s ~/.config/hyprland/tmux ~/.config/tmux
 # waybar
+rm ~/.config/waybar -r
+ln -s ~/.config/hyprland/waybar ~/.config/waybar
 # wofi
+rm ~/.config/wofi -r
+ln -s ~/.config/hyprland/wofi ~/.config/wofi
 # zathura
+rm ~/.config/zathura -r
+ln -s ~/.config/hyprland/zathura ~/.config/zathura
 # mimeapps.list
+rm ~/.config/mimeapps.list
+ln -s ~/.config/hyprland/mimeapps.list ~/.config/mimeapps.list
